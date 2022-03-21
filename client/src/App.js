8cdfbc623643e6cff0b4aa048eb6d39ae53d7fe5
Noa Vered Shalom <noavrd@gmail.com>
2022-03-21 17:30:21 +0200

    add Data

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
-import './App.css';
+import "./App.css";
 import { firebase } from "@firebase/app";
-// const firebase = require('firebase/app');
-require('firebase/firestore');
+import { useEffect, useState } from "react";
+import SingleOrder from "./components/SingleOrder";
+
+require("firebase/firestore");
 
 const firebaseConfig = {
   apiKey: "AIzaSyD8s2M120Eniqu3lozRZof0cAm0TSIUdF4",
@@ -9,33 +11,78 @@ const firebaseConfig = {
   projectId: "learn-78a20",
   storageBucket: "learn-78a20.appspot.com",
   messagingSenderId: "583048417423",
-  appId: "1:583048417423:web:c70cfdb48f1fcb46b6a0fa"
+  appId: "1:583048417423:web:c70cfdb48f1fcb46b6a0fa",
 };
 const app = firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
-db.settings({timestampUnsnapshots: true});
+db.settings({ timestampsInsnapshots: true });
+const orders = [];
 
 function App() {
-  
-  
+  const [allOrders, setAllOrders] = useState([]);
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [change, setChange] = useState(false);
+
   const addData = () => {
+    // ADD data to a collection
+    db.collection("cafes")
+      .add({
+        name,
+        city,
+      })
+      .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch((err) => {
+        console.error("Error adding document: ", err);
+      });
+    setChange(true);
+  };
 
-    db.collection("cafes").add({
-      name: "Rona Latte",
-      city: "Holon",
-    }).then((docRef) => {
-      console.log("Document written with ID: ", docRef.id);
-    }).catch((err) => {
-      console.error("Erroe adding document: ", err);
-    });
-  }
+  // GET the data from a collection
+  // can't do const collection = ... because it can take some time
 
+  const getData = () => {
+    db.collection("cafes")
+      .get()
+      .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          // get all the data from each document
+          // console.log(doc.data())
+          const data = {
+            name: doc.data().name,
+            city: doc.data().city,
+          };
+          orders.push(data);
+        });
+        setAllOrders(orders);
+      });
+  };
+
+  useEffect(() => {
+    console.log(1);
+    getData();
+  }, []);
+  useEffect(() => {
+    if (change) {
+      getData();
+      setChange(false);
+    }
+  }, [change]);
   return (
     <div>
-      <button onClick={()=>addData()}>Add</button>
+      <div>
+        <input placeholder="Name" onChange={(e) => setName(e.target.value)} />
+        <input placeholder="City" onChange={(e) => setCity(e.target.value)} />
+        <button onClick={() => addData()}>Add</button>
+      </div>
+      {orders &&
+        orders.map((order, i) => (
+          <SingleOrder name={order.name} city={order.city} key={i} />
+        ))}
     </div>
-   
   );
 }
 
---
+++ client/src/components/SingleOrder.js
@@ -0,0 +1,8 @@
+export default function singleOrder({ name, city }) {
+  return (
+    <div>
+      <span>{name} </span>
+      <span>{city}</span>
+    </div>
+  );
+}
